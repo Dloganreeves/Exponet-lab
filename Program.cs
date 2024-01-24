073fa1da042630d89e67a0cee9d1e8fc8244181a
Logan Reeves <loganreeves1@yahoo.com>
2024-01-24 16:03:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+
+namespace exponets_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!");
+            
+
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Please enter an integer ");
+                int usernum = int.Parse(Console.ReadLine());
+                if (usernum >= 1 && usernum < 1290)
+                {
+                    Console.WriteLine($"Header \t\t\t Double \t\t\t Triple");
+                    for (int i = 0; i < usernum; i++)
+                    {
+                        Console.WriteLine($"{i}\t\t\t {i * i}\t\t\t\t\t{i*i*i}");
+                        
+                    }
+
+                    Console.WriteLine("Do you want to contiue? y/n");
+                    string choice = Console.ReadLine();
+                    if (choice == "y".ToUpper())
+                    {
+                        repeat = true;
+                    }
+                    else if (choice == "n".ToUpper())
+                    {
+                        repeat = false;
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Please select a number greater than 0.");
+                }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
